23c662823b3d11b22b93700dabe41de134a2becf
Saad <m.saad.raza1999@gmail.com>
2021-09-06 18:47:37 -0400

    updated about us page ocmpoennts

--- src/components/about-us/mission.js
+++ src/components/about-us/mission.js
@@ -1,16 +1,13 @@
 import * as React from "react"
 
-const Mission = () => {
+const Mission = (props) => {
   return (
     <div class="tile is-6 is-parent" >
       <div class="tile is-child format-box" >
-        <h1 class="title is-2 hero-heading-border">Our Mission</h1>
-        <p>some regular text about this post or some excerpt tings, this is a bit longer exceprt so that
-          it goes onto the next line and well se what happens gonan next ok, ok</p>
+      <h1 class="title is-2 hero-heading-border">{props.data.heading}</h1>
+        <p>{props.data.blurb1}</p>
 
-        <p>shtings split into 2 paragprahss for more nicer tings
-          it goes onto the next line and well se what happens gonan next ok, ok</p>
-
+        <p>{props.data.blurb2}</p>
       </div>
     </div>
   )
