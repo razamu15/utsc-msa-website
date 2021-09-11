a3fafff2a3315cecd08b6fb687f4d67a6d7cc02f
Saad <m.saad.raza1999@gmail.com>
2021-09-11 18:12:54 -0400

    added inpage links in about-us sections

--- src/components/about-us/mission.js
+++ src/components/about-us/mission.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 const Mission = (props) => {
   return (
-    <div class="tile is-6 is-parent" >
+    <div class="tile is-6 is-parent" id="mission" >
       <div class="tile is-child format-box" >
       <h1 class="title is-2 hero-heading-border">{props.data.heading}</h1>
         <p>{props.data.blurb1}</p>
