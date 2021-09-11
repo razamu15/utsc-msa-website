597c69fe229ed1b466d9210e40880a6a7f24eeb3
Saad <m.saad.raza1999@gmail.com>
2021-09-11 14:52:48 -0400

    completed event template page

---
+++ gatsby-node.js
@@ -0,0 +1,82 @@
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  // Define a template for blog post
+  const eventComponent = path.resolve('./src/templates/event-template.js')
+  const programComponent = path.resolve('./src/templates/program-template.js')
+
+  const result = await graphql(
+    `
+      {
+        allContentfulEvent {
+          nodes {
+            slug
+          }
+        }
+        allContentfulProgram {
+          nodes {
+            slug
+          }
+        }
+      }
+    `
+  )
+
+  if (result.errors) {
+    reporter.panicOnBuild(
+      `There was an error loading your Contentful posts`,
+      result.errors
+    )
+    return
+  }
+
+  const events = result.data.allContentfulEvent.nodes
+  const programs = result.data.allContentfulProgram.nodes
+
+
+  // Create blog posts pages
+  // But only if there's at least one blog post found in Contentful
+  // `context` is available in the template as a prop and as a variable in GraphQL
+  events.forEach(ev => {
+    console.log(ev);
+
+    createPage({
+      path: `/events/${ev.slug}/`,
+      component: eventComponent,
+      context: {
+        slug: ev.slug,
+      },
+    })
+  })
+  programs.forEach(prog => {
+    console.log(prog)
+
+    createPage({
+      path: `/programs/${prog.slug}/`,
+      component: programComponent,
+      context: {
+        slug: prog.slug,
+      },
+    })
+  })
+
+  // if (posts.length > 0) {
+  //   posts.forEach((post, index) => {
+  //     const previousPostSlug = index === 0 ? null : posts[index - 1].slug
+  //     const nextPostSlug =
+  //       index === posts.length - 1 ? null : posts[index + 1].slug
+
+  //     createPage({
+  //       path: `/blog/${post.slug}/`,
+  //       component: blogPost,
+  //       context: {
+  //         slug: post.slug,
+  //         previousPostSlug,
+  //         nextPostSlug,
+  //       },
+  //     })
+  //   })
+  // }
+}
--- src/templates/event-template.js
+++ src/templates/event-template.js
@@ -1,21 +1,92 @@
 import * as React from "react"
 import { graphql } from "gatsby"
-import Layout from "../../components/layout"
+import Layout from "../components/layout"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 // markup
+const Detail = (props) => {
+  return (
+    <div class="field is-horizontal">
+      <div class="field-label is-normal">
+        <label class="label">{props.label}</label>
+      </div>
+      <div class="field-body">
+
+        <p class="control">
+          <input class="input is-static" type="text" value={props.value} readOnly />
+        </p>
+
+      </div>
+    </div>
+  )
+}
+
 const EventPage = (props) => {
   console.log(props)
+  const event = props.data.contentfulEvent
+  const back = 
+  <a class="button is-outlined is-inverted">
+    <span class="icon">
+      <FontAwesomeIcon icon="arrow-left" />
+    </span>
+    <span>All Events</span>
+  </a>
 
   return (
-    <Layout heading={"some"} subHeading={"thing"} socials={false} size="medium">
-      random text ah in a singular event page ee
+    <Layout heading="Events" subHeading={back} socials={false} size="halfheight">
+      <section class="hero is-medium" >
+        <div class="hero-body" style={{ padding: "4rem 5rem 4rem 5rem" }}>
+          <div class="tile is-ancestor" style={{ flexWrap: "wrap" }}>
+            <div class="tile is-3 is-parent" >
+              <div class="tile is-child format-box" >
+                <h1 class="title is-2">{event.title}</h1>
+              </div>
+            </div>
+            <div id="mission-img" class="tile is-9 is-parent" style={{ justifyContent: "center" }}>
+              <div class="img-tile tile is-child">
+                <img
+                  src={event.poster.file.url} />
+              </div>
+            </div>
+            <div class="tile is-4 is-parent" >
+              <div class="tile is-child format-box" style={{ justifyContent: "flex-start" }} >
+                <h1 class="title is-3 hero-heading-border">Details</h1>
+                <Detail label="Date" value={event.day} />
+                <Detail label="Time" value={event.time} />
+                <Detail label="Registration Required" value={event.registration ? "Yes" : "No"} />
+                <Detail label="Location" value={event.location} />
+              </div>
+            </div>
+            <div class="tile is-8 is-parent" >
+              <div class="tile is-child format-box" style={{ justifyContent: "flex-start" }} >
+                <h1 class="title is-3 hero-heading-border">Description</h1>
+                <p>{event.description.description}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
 
 export default EventPage
 
-// export const query = graphql`
-//   {
-//   }
-// `
+export const query = graphql`
+  query SingleEventPage($slug: String) {
+    contentfulEvent(slug: { eq: $slug}) {
+      day: date(formatString: "ddd, MMM Do")
+      time: date(formatString: "hh:mma")
+      registration
+      title
+      poster {
+        file {
+          url
+        }
+      }
+      description {
+        description
+      }
+    }
+  }
+`
